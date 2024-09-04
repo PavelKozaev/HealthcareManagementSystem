5999acb6b721cdeca5131f63026a0e5ddca88891
PavelKozaev <pkozaev221299@gmail.com>
2024-09-04 17:03:17 +0300

    Register application services and repositories

--- HealthcareManagementSystem.Api/Program.cs
+++ HealthcareManagementSystem.Api/Program.cs
@@ -1,8 +1,31 @@
+using HealthcareManagementSystem.Application.Mappings;
+using HealthcareManagementSystem.Application.Services.Interfaces;
+using HealthcareManagementSystem.Application.Services;
+using HealthcareManagementSystem.Core.Interfaces;
+using HealthcareManagementSystem.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using HealthcareManagementSystem.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Add DbContext with SQL Server provider
+builder.Services.AddDbContext<HealthcareDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add AutoMapper with the specified profiles
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+// Register application services and repositories
+builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
+builder.Services.AddScoped<IPatientRepository, PatientRepository>();
+builder.Services.AddScoped<IDoctorService, DoctorService>();
+builder.Services.AddScoped<IPatientService, PatientService>();
+
+// Add controllers
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
