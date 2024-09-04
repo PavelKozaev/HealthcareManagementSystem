048477b60cf945a6905c7bf8138beec1300f8ff9
PavelKozaev <pkozaev221299@gmail.com>
2024-09-04 16:49:15 +0300

    Add mappings

---
+++ HealthcareManagementSystem.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using HealthcareManagementSystem.Core.Entities;
+using HealthcareManagementSystem.Application.DTOs;
+
+namespace HealthcareManagementSystem.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {            
+            CreateMap<Doctor, DoctorDto>()
+                .ForMember(dest => dest.OfficeNumber, opt => opt.MapFrom(src => src.Office.Number))
+                .ForMember(dest => dest.SpecializationName, opt => opt.MapFrom(src => src.Specialization.Name))
+                .ForMember(dest => dest.PlotNumber, opt => opt.MapFrom(src => src.Plot.Number));
+
+            CreateMap<Doctor, DoctorForEditDto>().ReverseMap();
+
+            CreateMap<Patient, PatientDto>()
+                .ForMember(dest => dest.PlotNumber, opt => opt.MapFrom(src => src.Plot.Number));
+
+            CreateMap<Patient, PatientForEditDto>().ReverseMap();            
+        }
+    }
+}
